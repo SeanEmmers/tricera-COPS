7f087d8d58ddf25ca8ad74be4dd54d829e15427e
Sean Phillips <seanphillips@seans-air.lan>
2021-08-11 16:52:38 +0100

    testing creating of landing-room

---
+++ src/components/landing-room/LandingRoom.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const LandingRoom = () => {
+  return (
+    <div className='LandingRoom'>
+      <p>Welcome to the Dino mystery!</p>
+
+    </div>
+  )
+}
+
+export default LandingRoom;
