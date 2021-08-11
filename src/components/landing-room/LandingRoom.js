af9441566ee8bf23bb207aad9a6d26dced25b1c0
Sean Phillips <seanphillips@seans-air.lan>
2021-08-11 17:31:02 +0100

    creating final pop up

--- src/components/landing-room/LandingRoom.js
+++ src/components/landing-room/LandingRoom.js
@@ -1,16 +1,24 @@
 import React from "react";
 
+class EndingText extends React.Component {
+  render() {
+    return (
+      <p className='EndingText'>
+        {this.props.message}
+      </p>
+    )
+  }
+}
+
 const LandingRoom = () => {
   return (
     <div className='LandingRoom'>
       <p>Welcome to the Dino mystery!</p> 
       <form>
-        <button>T-rex is the killer?</button> 
+        <button OnClick = {EndingText.props.message = 'You win'}>T-rex is the killer?</button> 
         <button>Asteroid is the killer?</button>
       </form>
-        <p className='EndingText'>
-          
-        </p>
+      <EndingText />
     </div>
   )
 }
