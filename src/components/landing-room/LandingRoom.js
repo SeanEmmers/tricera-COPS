51c03ff2b5cc6d2728eaee6682a3313067e70001
Sean Phillips <seanphillips@seans-air.lan>
2021-08-11 17:02:22 +0100

    creating first elements

--- src/components/landing-room/LandingRoom.js
+++ src/components/landing-room/LandingRoom.js
@@ -3,8 +3,14 @@ import React from "react";
 const LandingRoom = () => {
   return (
     <div className='LandingRoom'>
-      <p>Welcome to the Dino mystery!</p>
-
+      <p>Welcome to the Dino mystery!</p> 
+      <form>
+        <button>T-rex is the killer?</button> 
+        <button>Asteroid is the killer?</button>
+      </form>
+        <p className='EndingText'>
+          
+        </p>
     </div>
   )
 }
