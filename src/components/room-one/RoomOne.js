24cdb544a4ccc5986c415a39aa3b9bef72237043
Sean Phillips <seanphillips@seans-air.lan>
2021-08-12 11:15:36 +0100

    adding first room image

---
+++ src/components/room-one/RoomOne.js
@@ -0,0 +1,14 @@
+import React from "react";
+import RoomOneImage from "./room-one-images/RoomOneImage.jpeg";
+
+// import "../../App.css";
+
+const RoomOne = () => {
+  return (
+    <div class="container">
+      <img src={RoomOneImage}  height="90%" width="80%" ></img>
+    </div>
+  )
+}
+
+export default RoomOne;
