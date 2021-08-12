0b6fd5b112ea5ce4365a0b360de9997cc41d912d
Sean Phillips <seanphillips@seans-air.lan>
2021-08-12 14:19:38 +0100

    first room completed

--- src/components/landing-room/LandingRoom.js
+++ src/components/landing-room/LandingRoom.js
@@ -1,23 +1,23 @@
 import React from "react";
 
 class EndingText extends React.Component {
-  render() {
-    return (
-      <p className='EndingText'>
-        {this.props.message}
-      </p>
-    )
+
+  onButtonClickHandler = (props) => {
+    window.alert(props)
   }
-}
+
+  render(){ 
+    return(<div className="EndingText">
+      <button onClick={() => this.onButtonClickHandler('You win!!')}>T-Rex is the killer?</button>
+      <button onClick={() => this.onButtonClickHandler('You lose!!')}>Asteroid is the killer?</button>
+    </div>);
+  }
+};
 
 const LandingRoom = () => {
   return (
     <div className='LandingRoom'>
-      <p>Welcome to the Dino mystery!</p> 
-      <form>
-        <button OnClick = {EndingText.props.message = 'You win'}>T-rex is the killer?</button> 
-        <button>Asteroid is the killer?</button>
-      </form>
+      <p>Welcome to the Dino mystery!</p>
       <EndingText />
     </div>
   )
