29ced751e68ce097d496a529d586ac81a9d470f7
Sean Phillips <seanphillips@seans-air.lan>
2021-08-13 11:59:49 +0100

    ready for merge

--- src/components/landing-room/LandingRoom.js
+++ src/components/landing-room/LandingRoom.js
@@ -47,7 +47,7 @@ const Door = () => {
 const LandingRoom = () => {
   return (
     <div className='LandingRoom'>
-      <div className="container">
+      <div className="parent">
         <img className="backgroundImage" src={RoomOneImage} alt="TriceraCop" />
         <Door/>
         <SpeechBubble/>
@@ -55,19 +55,5 @@ const LandingRoom = () => {
     </div>
   )
 }
-
-// class EndingText extends React.Component {
-
-//   onButtonClickHandler = (props) => {
-//     window.alert(props)
-//   }
-
-//   render(){ 
-//     return(<div className="EndingText">
-//       <button onClick={() => this.onButtonClickHandler('You win!!')}>T-Rex is the killer?</button>
-//       <button onClick={() => this.onButtonClickHandler('You lose!!')}>Asteroid is the killer?</button>
-//     </div>);
-//   }
-// };
 
 export default LandingRoom;
