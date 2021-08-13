60a3a8f3f8b502d3048e74a4f08518ac16c5d0b1
Sean Phillips <seanphillips@seans-air.lan>
2021-08-13 16:37:15 +0100

    new jar image

--- src/components/landing-room/LandingRoom.js
+++ src/components/landing-room/LandingRoom.js
@@ -1,10 +1,10 @@
 import React from "react";
 import RoomOneImage from "./room-one-images/RoomOneImage.jpeg";
-import OrangeDoor from "./room-one-images/DoorOrangeCropped.png";
+import DoorGlow from "./room-one-images/DoorOrangeCropped.png";
 import Popup from "./PopUp";
 import { useState } from 'react';
-import JarCropped from "./room-one-images/jar-cropped.jpeg";
-import DoorImg from "./room-one-images/CroppedDoorFinal.png";
+import JarGlow from "./room-one-images/JarCroppedFinal.png";
+import DoorBasic from "./room-one-images/CroppedDoorFinal.png";
 import JarBasic from "./room-one-images/CroppedJarFinal.png";
 
 class SpeechBubble extends React.Component {
@@ -33,7 +33,7 @@ class SpeechBubble extends React.Component {
 const Jar = () => {
 
   return(
-    <img className="jarOutline" src={JarBasic} alt="Jar" onMouseOver={e => e.currentTarget.src = JarCropped } onMouseOut={e => e.currentTarget.src = JarBasic }/>
+    <img className="jarOutline" src={JarBasic} alt="Jar" onMouseOver={e => e.currentTarget.src = JarGlow } onMouseOut={e => e.currentTarget.src = JarBasic }/>
   )
 }
 
@@ -42,7 +42,7 @@ const Door = () => {
 
   return(
     <div>
-     <img className="doorOutline" onClick={() => setButtonPopup(true)} src={DoorImg}>
+     <img className="doorOutline" onClick={() => setButtonPopup(true)} src={DoorBasic} onMouseOver={e => e.currentTarget.src = DoorGlow} onMouseOut={e => e.currentTarget.src = DoorBasic } alt="OrangeDoor">
       </img>
       <Popup trigger={buttonPopUp} setTrigger={setButtonPopup}>
         <div className="EndingText">
