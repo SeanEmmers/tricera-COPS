dd0f98497296edd932365b48b59e853efca7d776
Camilla <camilla.lima000@gmail.com>
2021-08-15 15:00:55 +0100

    Added Navbar, Inventory, styled buttoms

--- src/App.js
+++ src/App.js
@@ -2,8 +2,12 @@ import React from "react";
 import "./App.css";
 import LandingRoom from "./components/landing-room/LandingRoom";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {Navbar} from "react-bootstrap";
+import {Container, Row, Col} from "react-bootstrap";
+import Inventory from "./components/landing-room/Inventory";
 
 function App() {
+ 
   const [data, setData] = React.useState(null);
 
   // React.useEffect(() => {
@@ -14,7 +18,28 @@ function App() {
 
   return (
     <div className="App">
-      <LandingRoom />
+      <Navbar fixed="top" class="navbar" variant="dark">
+        <Container>
+          <Navbar.Brand>TriceraCops</Navbar.Brand>
+        </Container>
+      </Navbar>
+      <br />
+      <br />
+      <Container>
+        <br />
+        <br />
+        <Row>
+          <Col lg="2">
+            <Inventory />
+          </Col>
+
+          <Col>
+            <LandingRoom />
+          </Col>
+        </Row>
+        <br />
+        <br />
+      </Container>
     </div>
   );
 }
