2471ea683249417401eed6dd5223eedf7aabd03a
Camilla <camilla.lima000@gmail.com>
2021-08-17 20:55:40 +0100

    Added Homepage and routes, fully functioning let's play button and image placeholder placed

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import React from "react";
 import "./App.css";
 import LandingRoom from "./components/landing-room/LandingRoom";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Navbar} from "react-bootstrap";
-import {Container, Row, Col} from "react-bootstrap";
+// import {Navbar} from "react-bootstrap";
+import {Navbar,Container} from "react-bootstrap";
+import {Route, Link} from 'react-router-dom'
+import Home from "./components/homepage/home.js"
 
 
 function App() {
@@ -20,15 +22,29 @@ function App() {
     <div className="App">
       <Navbar fixed="top" class="navbar" variant="dark">
         <Container>
-          <Navbar.Brand>TriceraCops</Navbar.Brand>
+          <Navbar.Brand href="/">
+            <img
+              
+              src="https://www.maxpixel.net/static/photo/1x/Dino-Cartoon-Triceratops-Dinosaur-Green-Portrait-306331.png"
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+            />{" "}
+           TriceraCops
+          </Navbar.Brand>
+          <Navbar.Text>
+           About Us?
+          </Navbar.Text>
         </Container>
       </Navbar>
       <br />
       <br />
       <Container>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/play" component={LandingRoom} />
         <br />
         <br />
-        <LandingRoom />
+        {/* <LandingRoom /> */}
       </Container>
     </div>
   );
---
+++ src/components/homepage/home.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+import { Container, Button, Col, Row } from "react-bootstrap";
+import "./home.css";
+
+const Home = () => {
+  return (
+    <div>
+      <Container>
+        <Row>
+          <Col>
+            <br />
+            <br />
+            <Link to="/play">
+              <img
+                src="https://static3.srcdn.com/wordpress/wp-content/uploads/2021/02/Hamilton-Schuyler-Sisters.jpg?q=50&fit=crop&w=960&h=500&dpr=1.5"
+                alt=""
+              />
+              <br />
+              <br />
+              <div className="play">
+                <Button variant="danger" size="lg">
+                  Let's Play, Bitch!!
+                </Button>
+              </div>
+            </Link>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+}
+
+
+export default Home;
