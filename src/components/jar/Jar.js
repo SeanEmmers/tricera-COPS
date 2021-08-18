ec90c232cf257ec5651951a962ee6588ed7efe5c
Masako Gray <masakogray@hotmail.co.uk>
2021-08-18 22:00:40 +0100

    softening design for popup

--- src/components/jar/Jar.js
+++ src/components/jar/Jar.js
@@ -27,7 +27,9 @@ const Jar = ({jarMethod}) => {
       <img className="jarOutline" onClick={() => ClickHandler()} src={JarBasic} alt="Jar" onMouseOver={e => e.currentTarget.src = JarGlow } onMouseOut={e => e.currentTarget.src = JarBasic }/>
       <Popup show={showPopup} setShow={setButtonPopup}>
         <div>
-          <p>You notice a golden vase on the mantelpiece. Upon closer inspection, it seems that there is something inside. You shake it out and a blood-stained tooth falls on the floor. Curious? Check you inventory!</p>
+          <p>You notice a golden vase on the mantelpiece. Upon closer inspection, it seems that there is something inside. You shake it out and a blood-stained tooth falls on the floor.
+          <p>
+          </p>Curious? Check you inventory!</p>
         </div>
       </Popup>
       <SpeechBubbleReuse display={speechBubble} showBubble={setSpeechbubble} words={bubbleText}>
