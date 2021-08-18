87fda9bd0535c4eacf475f3ca4f1f5d259dfa3c5
Sean Phillips <seanphillips@seans-air.lan>
2021-08-18 15:50:33 +0100

    dino speech bubble

--- src/components/dino-cop/DinoSuspect.js
+++ src/components/dino-cop/DinoSuspect.js
@@ -3,16 +3,24 @@ import { useState } from 'react';
 import DinoImg from "./dino-images/triceCOP.png";
 import "./Dino.css";
 import Popup from "../popup/PopUp";
+import SpeechBubbleReuse from "../speech-bubble/SpeechBubbleReuse";
 
 const DinoSuspect = (props) => {
   const [showPopup, setButtonPopup] = useState(false);
+  const [speechBubble, setSpeechbubble] = useState(false);
 
   const clickHandler = () => {
     setButtonPopup(true);
     props.showSuspects(false);
     props.showDoor(true);
     props.showKey(true);
-   
+    setSpeechbubble(true);
+  }
+
+  const bubbleText = () => {
+    return(
+      "Argghhh! I can't believe you caught me RAAWWWWRRR 🦖"
+    )
   }
 
   return (
@@ -23,6 +31,8 @@ const DinoSuspect = (props) => {
           <p>Congratulations, you caught the killer and recieved a key !! </p>
         </div>
       </Popup>
+      <SpeechBubbleReuse display={speechBubble} showBubble={setSpeechbubble} words={bubbleText}>
+      </SpeechBubbleReuse>
     </div>
   );
 };
