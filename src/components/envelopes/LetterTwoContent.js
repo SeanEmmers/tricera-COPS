a177d76971589243d7541a76bc7510983f84a5c9
Sean Phillips <seanphillips9696@gmail.com>
2021-08-22 18:30:44 +0100

    spelling

--- src/components/envelopes/LetterTwoContent.js
+++ src/components/envelopes/LetterTwoContent.js
@@ -5,7 +5,7 @@ const LetterTwoContent = () => {
   return(
     <div>
       <p>Dear Andy,</p>
-      <p>I hope your relationship is improving. I know you were considering separating the last time we spoke but I urge youto reconsider. Priya is a special woman, and you have loved her for as long as I've known you. I think you will regret your decision if you do anything rash.</p>
+      <p>I hope your relationship is improving. I know you were considering separating the last time we spoke but I urge you to reconsider. Priya is a special woman, and you have loved her for as long as I've known you. I think you will regret your decision if you do anything rash.</p>
       <p>This rise in criticism of inter-species relationships between dinosaurs is as divisive as it is regressive. It is no more than populists trying to win cheap votes and line their pockets, I pray you pay such talk no notice. Unless you think we should go back to wearing no clothes like we were doing millions of years ago. Now carnivores and herbivores live side by side as the dearest of friends, should we revers all of this and return to those animalistic notions of division? I think not.</p>
       <p>Please forgive my tangential ramblings, as a university professor, sometimes I think it's all I'm good for. Whatever you do, I'll be here to support you, but watch that temper of yours. It's always getting you into regrettable situations.</p>
       <p>Yours truly,</p>
